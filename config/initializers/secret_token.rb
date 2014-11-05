2174ee23754b892268ca660457fe0472bc37e0e2
Alan Cumiskey <zilk@campus.ie>
2014-11-05 21:13:49 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Qinteresting::Application.config.secret_key_base = 'dc4382e85b4fd2c9fcabd19514ebf8918a55095dc280248374f975c45efc9a542458579bf2d3edc15f2e4884265829661bfe63c7c686ce07b6ae55a60c16f270'
