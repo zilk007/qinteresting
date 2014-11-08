f4d117e55359dcb63815568bc8be92e2cabc8bf4
Alan Cumiskey <zilk@campus.ie>
2014-11-08 00:13:31 +0000

    added sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Qinteresting::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = {host: 'http://email-screen.herokuapp.com/'}
+  config.action_mailer.default_url_options = {host: 'email-screen.herokuapp.com'}
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
